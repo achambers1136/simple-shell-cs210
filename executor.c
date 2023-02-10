a67f73b4bf1ac9800050f33c4fbe49f3c09775ce
shmove <leahlddl@gmail.com>
2023-02-10 15:53:00 +0000

    Execution of external commands

--- executor.c
+++ executor.c
@@ -2,6 +2,44 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "parser.h"
+
+int shell_exec_ext(int argc, char* argv[]) {
+    if (argc < 0) return 1;
+    if (argc == 0) return 0;
+
+    pid_t res = fork();
+
+    if (res < 0) {
+        fprintf(stderr, "Couldn't spawn a child process.\n");
+        return 1;
+    }
+
+    if (res == 0) {
+        char* PATH_spl;
+        char* rest = getenv("PATH"); // likely replace this with global path once implemented, doesn't support current dir
+        int status = -1;
+
+        while ((PATH_spl = strtok_r(rest, ":", &rest)) && status == -1) {
+            char* path = strdup(PATH_spl);
+                  path = strcat( strcat(path, "/"), argv[0] );
+            //printf("[trying %s]\n", path);
+            status = execv(path, argv);
+        } 
+
+        //printf("Child process finished. (status %d)\n", status);
+        if (status != 0) fprintf(stderr, "'%s' is not recognised as a file or internal/external command.\n", argv[0]);
+        exit(status);
+    } else {
+        wait(NULL);
+        printf("\n");
+    }
+
+    return 0;
+}
 
 int shell_exec(int argc, char* argv[]) {
     if (argc < 0) return 1; // err
@@ -14,6 +52,5 @@ int shell_exec(int argc, char* argv[]) {
     if (strcmp(argv[0], "exit") == 0) return 70;
 
     /* Else execute command as an external process */
-
-    return 0; // default
+    else return shell_exec_ext(argc, argv);
 }
