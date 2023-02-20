68adb89f79622b74e8279145381c035378052d8e
shmove <matthewliddle03@gmail.com>
2023-02-20 18:08:53 +0000

    Implemented excessive input rejection

--- main.c
+++ main.c
@@ -20,11 +20,12 @@ int main ( int argc, char* argv[] ) {
 
     printf("$w4g$h311 initialised.\n\n");
     int status = 0;
+    int rejecting = 0; // bool to track if shell should loop until end of excessive input to reject all
 
     /* Do while shell has not terminated */
     while(1) {
         /* Display prompt */
-        printf("> ");
+        if (!rejecting) printf("> ");
         char inp[512];
 
         /* Read and parse user input */
@@ -35,6 +36,16 @@ int main ( int argc, char* argv[] ) {
         
         if (strcmp(inp, "\n") == 0) continue; // reprompts if empty line
         
+        char* newline_ptr = strstr(inp, "\n");
+        if (newline_ptr == NULL) {
+            rejecting = 1;                                                      // begins rejection if no newline (ie input > 512 chars)
+            continue; 
+        } else if (rejecting == 1) { 
+            rejecting = 0;                                                      // finish rejection if newline is present but still within rejection loop
+            fprintf(stderr, "ERROR: Input exceeds character limit (512).\n");
+            continue;
+        }
+
         inp[strcspn(inp, "\n")] = 0; // removes newline from end
 
         char* cmds[50];                                 // stores commands split by ';'
